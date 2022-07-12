dc3cf68b0bde34471d34c93802f05c6b03cbd0ea
narinda giriputra <narinda.giriputra@gmail.com>
2022-07-12 11:07:24 +0800

    add user detail

--- routes/admin.js
+++ routes/admin.js
@@ -18,4 +18,22 @@ router.get('/users', function(req, res, next) {
   res.render('./admin/userList', {users, title: 'foo'});
 });
 
+router.get('/user/detail', function(req, res, next) {
+  const title = 'user detail';
+  const header = 'user detail';
+  const selectUser = {
+    id: 1,
+    username: 'foo',
+    role: 'admin',
+    email: 'bar@example.com',
+  }
+  const bio = {
+    id: 1,
+    firstname: 'John',
+    lastname: 'Smith',
+    gender: 'male'
+  };
+  res.render('./admin/userDetail', { title, header, selectUser, bio });
+});
+
 module.exports = router;
