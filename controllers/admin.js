f1d807ed6ccad741670cdb198443d652026c3f3d
narinda giriputra <narinda.giriputra@gmail.com>
2022-07-12 12:14:12 +0800

    show user details from database

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,21 @@ const userList = async (req, res, next) => {
   }
 }
 
+const userDetails = async (req, res) => {
+  try {
+    const title = 'User details';
+    const header = 'User details';
+    const id = req.params.id;
+    const user = await User.findOne({where: {id}})
+    const bio = await Biodatum.findOne({where: {UserId: id}})
+    res.render('./admin/userDetail', {title, header, user, bio})
+  } catch (error) {
+    res.send(error)    
+  }
+}
+
 
 module.exports = {
-  userList
+  userList,
+  userDetails
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const { userList } = require('../controllers/admin');
+const { userList, userDetails } = require('../controllers/admin');
 
-/* GET users listing. */
+//? admin routes endpoints
+
 router.get('/users', userList);
+router.get('/user/:id/detail', userDetails);
 
-router.get('/user/detail', function(req, res, next) {
-  const title = 'user detail';
-  const header = 'user detail';
-  const selectUser = {
-    id: 1,
-    username: 'foo',
-    role: 'admin',
-    email: 'bar@example.com',
-  }
-  const bio = {
-    id: 1,
-    firstname: 'John',
-    lastname: 'Smith',
-    gender: 'male'
-  };
-  res.render('./admin/userDetail', { title, header, selectUser, bio });
-});
 
 module.exports = router;
